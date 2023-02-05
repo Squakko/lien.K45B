bbb4e598ff3ec9cc2f8b27ae45640f019c5ab8bc
Minh Liên <96945297+Squakko@users.noreply.github.com>
2023-02-05 12:05:50 +0000

    make sure that document is displayable

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 $(document).ready(function () {
-   RunAnimation();
+    setTimeout(() => {
+        RunAnimation()
+    }, 1000);
 })
 
 function RunAnimation() {
     $('.text-play-top').delay(500).animate({
         left: '-=30vw',
-    }, 300, '', function() {
+    }, 300, ''/**swing or linear */, function() {
         $('html').css({'background-color': '#000000'});
         $('.text').css({'color': 'white'});
             $('.masker, .text').animate({
-            },700, /*SplitAnimation()*/)
+            },700, SplitAnimation())
     })
 
     $('.text-play-bottom').delay(400).animate({
@@ -18,13 +20,19 @@ function RunAnimation() {
 }
 
 function SplitAnimation() {
-
-
-
+// tách 2 mảng text từ play, cho 2 cái chạy ngược phía nhau
     $('.text-play-top').animate({
-        top: '-=100vh',
-    },800)
+        top: '-=1.28vh',
+        left: '-=3vw'
+    },400)
+
     $('.text-play-bottom').animate({
-        top: '+=100vh',
-    },800)
+        top: '+=1.28vh',
+    },400)
+// page use as breaker
+    // $('.page').animate({
+    //     left: '0',
+    // }, fast)
+
+
 }
