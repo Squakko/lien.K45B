33ba369d9d6956bb4c4f531a3dd3572d4a6ccd0c
Minh Liên <96945297+Squakko@users.noreply.github.com>
2023-02-09 18:53:54 +0000

    maybe good

--- js/main.js
+++ js/main.js
@@ -3,15 +3,24 @@
 //         RunAnimation()
 //     }, 1000);
 // })
-
+// var viewportWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+var isAnimated = 0;
+var viewportWidth = window.innerWidth;
+// const browser = navigator.userAgent; //cái này để test trình duyệt
+// alert(viewportWidth + browser);
 function LaunchPage() {
-    setTimeout(() => {
-        LaunchSound();
-    }, 3);
 
-    setTimeout(() => {
-        RunAnimation()
-    }, 30);
+    if (isAnimated === 0) {
+        isAnimated = 1;
+        setTimeout(() => {
+            // document.getElementsByTagName("masker")[0].removeAttribute("onclick");
+            LaunchSound();
+        }, 3);
+        
+        setTimeout(() => {
+            RunAnimation()
+        }, 3);
+    }
 }
 
 
@@ -20,11 +29,11 @@ function RunAnimation() {
     var duration = 700;
     $('.text-play-top').delay(500).animate({
         left: toLeft,
-    }, duration, ''/**swing or linear */, function() {
-        $('html').css({'background-color': '#000000'});
-        $('.text').css({'color': 'white'});
-            $('.masker, .text').animate({
-            },700, SplitAnimation())
+    }, duration, ''/**swing or linear */, function () {
+        $('html').css({ 'background-color': '#000000' });
+        $('.text').css({ 'color': 'white' });
+        $('.masker, .text').animate({
+        }, 700, SplitAnimation())
     })
 
     $('.text-play-bottom').delay(400).animate({
@@ -33,24 +42,44 @@ function RunAnimation() {
 }
 
 function SplitAnimation() {
-// tách 2 mảng text từ play, cho 2 cái chạy ngược phía nhau
-var duration = 250;
-$('.text-play-top').animate({
-        top: '-=1.6rem',
-        left: '-=6vw'
-    },duration)
+    // tách 2 mảng text từ play, cho 2 cái chạy ngược phía nhau
+    var duration = 250;
 
-    $('.text-play-bottom').animate({
-        top: '+=1.6rem',
-        left: '+=4vw'
-    },duration)
-// page use as breaker
-$('.page').css({
-    display: 'block',
-})
+    if (viewportWidth < 1000) {
+        $('.text-play-top').animate({
+            top: '-=4rem',
+            left: '-=6vw'
+        }, duration)
+    
+        $('.text-play-bottom').animate({
+            top: '+=4rem',
+            left: '+=4vw'
+        }, duration)
+    } else {
+        $('.text-play-top').animate({
+            top: '-=1.6rem',
+            left: '-=6vw'
+        }, duration)
+    
+        $('.text-play-bottom').animate({
+            top: '+=1.6rem',
+            left: '+=4vw'
+        }, duration)
+    }
+    // page use as breaker
+    $('.page').css({
+        display: 'block',
+    })
+
+    // if (!navigator.userAgent.indexOf('Firefox')) { cái này xóa đi cũng được
+    //     $('.page').css({
+    //         top: 'calc(50% - 100px)'
+    //     })
+    // }
+
     $('.page').animate({
         left: '0px',
-    }, 170, function() {
+    }, 170, function () {
         var duration = 500;
         var duration2 = 700;
         var delay = 300;
@@ -65,7 +94,7 @@ $('.page').css({
             $('.text-play-top').animate({
                 top: '-=100vh',
             }, duration2)
-            
+
             $('.text-play-bottom').animate({
                 top: '+=100vh',
             }, duration2)
@@ -81,5 +110,6 @@ $('.page').css({
 })
 
 function LaunchSound() {
+
     $('.launchSound').trigger('play');
 }
